d506156b044b2b4e5e2759642c5f6d3529143593
alahundraa <33134480+alahundraa@users.noreply.github.com>
2021-11-19 12:40:32 +0400

    task_2

---
+++ task_2/task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Вячеслав";
+            Console.Write("Введите имя: ");
+            string str = Console.ReadLine();
+            if (name == str)
+                Console.WriteLine("Привет, Вячеслав");
+            else
+                Console.WriteLine("Нет такого имени");
+        }
+    }
+}
